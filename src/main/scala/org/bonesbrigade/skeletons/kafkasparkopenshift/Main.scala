3028434611dd8d198fdcc7761c17b355a8f5fafc
michael mccune <msm@opbstudios.com>
2018-10-07 17:30:57 -0400

    wrap the env variable functions

--- src/main/scala/org/bonesbrigade/skeletons/kafkasparkopenshift/Main.scala
+++ src/main/scala/org/bonesbrigade/skeletons/kafkasparkopenshift/Main.scala
@@ -4,8 +4,15 @@ import scala.sys
 
 object Main {
 
+  def getEnv(env: String, default: String) : String = {
+    sys.env get env match {
+      case Some(s) => s
+      case None => default
+    }
+  }
+
   def main(args: Array[String]) : Unit = {
-    val intopic = sys.env["KAFKA_IN_TOPIC"]
+    val intopic = getEnv("KAFKA_IN_TOPIC")
     println("kafka in topic " + intopic)
   }
 
